b5da8d0727fee5721bb80cf631712ab474f6c04a
Alan Vieceli <alansvieceli@gmail.com>
2019-06-03 19:39:06 -0300

    Configurando o Servidor com o Express

---
+++ README.md
@@ -0,0 +1,9 @@
+
+npm i --save -E body-parser@1.19.0 express@4.17.1 mongoose@5.5.12 node-restful@0.2.5 pm2@3.5.1
+
+npm -i --save-dev -E nodemon@1.19.1
+
+
+npm run dev
+
+npm run production
---
+++ src/config/server.js
@@ -0,0 +1,14 @@
+const port = 3003
+
+//Parser do corpo da requisição, Formulario, JSON, XML
+const bodyParser = require("body-parser");
+//Servidor web que roda em cima do node
+const express = require("express");
+const server = express();
+
+server.use(bodyParser.urlencoded({extended: true})); //formulario
+server.use(bodyParser.json());
+
+server.listen(port, function(){
+    console.log("BACKEND está rodando, joinha!!")
+});
